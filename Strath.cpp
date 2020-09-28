21ef74cbd29d5950802d59ce61e2ac20ee3b96cd
rashid twahir <rtwahir1@gmail.com>
2020-09-28 21:37:00 +0300

    Add files via upload

---
+++ Strath.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+// using namespace std; has been replaced by std::
+
+int main () {
+	
+	std:: cout << " Strathmore Univerity Vision\n"; // Heading
+	std:: cout << " ........................\n";   // Line Seperator
+	
+	// Below is the Paragraph
+	std:: cout<< "To become a leading out- come driven entrepreneurial \n"
+	          << "research University by translating our excellence into\n"
+      		  <<  "major contribution to culture, economic well-being and \n" 
+			  << "quality of life.\n";
+
+	
+	return 0;
+}
